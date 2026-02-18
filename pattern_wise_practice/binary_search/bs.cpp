9a7f82c660b3b892a6aa954b637b044e26e9b784
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-02-18 21:43:05 +0530

    Classic Binary Search

---
+++ pattern_wise_practice/binary_search/bs.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(std::vector<int>& nums, int target) {
+    int low = 0, high = nums.size() - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+    int result = search(nums, target);
+    cout << result << endl;
+    return 0;
+}
