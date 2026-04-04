4cec78ba253d50a39a2d00dccb8c744b5e96330b
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-04-04 15:30:54 +0530

    DSA Sheet R&B

--- pattern_wise_practice/recursion&backtrack/lps.cpp
+++ pattern_wise_practice/recursion&backtrack/lps.cpp
@@ -1,18 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int helper(string s,int i,int j){
-    if(i>j)return 0;
-    if(i==j)return 1;
-    if(s[i]==s[j]){
-        return 2+helper(s,i+1,j-1);
+int arr[1001][1001];
+
+int helper(string s, int i, int j)
+{
+    if (i > j)
+        return 0;
+    
+    if (i == j)
+        return 1;
+    if(arr[i][j]!=-1)return arr[i][j];
+    if (s[i] == s[j])
+    {
+        return arr[i][j]=2 + helper(s, i + 1, j - 1);
     }
-    return max(helper(s,i+1,j),helper(s,i,j-1));
+    return arr[i][j]=max(helper(s, i + 1, j), helper(s, i, j - 1));
 }
-int main(){
-    string s="aabcdeecdar";
-    int ans=helper(s,0,s.size()-1);
-    cout<<ans;
+int main()
+{
+    string s = "aabcdeecdar";
+    for (int i = 0; i < 1001; i++)
+    {
+        for (int j = 0; j < 1001; j++)
+        {
+            arr[i][j] = -1;
+        }
+    }
+    helper(s, 0, s.size() - 1);
+    cout << arr[0][s.size() - 1];
     return 0;
 }
