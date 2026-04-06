6a6ee9c7386aa4e07c014b7c5b9f6b33d7855704
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-04-06 17:33:23 +0530

    DSA Sheet R&B

--- pattern_wise_practice/recursion&backtrack/houserobber.cpp
+++ pattern_wise_practice/recursion&backtrack/houserobber.cpp
@@ -1,14 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+int dp[1001];
 
 int hrob(vector<int>&arr,int n){
     if(n>=arr.size())return 0;
+    if(dp[n]!=-1)return dp[n];
     int t=arr[n]+hrob(arr,n+2);
     int l=hrob(arr,n+1);
-    return max(t,l);
+    return dp[n]=max(t,l);
 }
 int main(){
+    memset(dp,-1,sizeof(dp));
     vector<int>arr={10,12,14,16,18};
-    cout<<hrob(arr,0);
+    hrob(arr,0);
+    cout<<dp[0];
     return 0;
 }
