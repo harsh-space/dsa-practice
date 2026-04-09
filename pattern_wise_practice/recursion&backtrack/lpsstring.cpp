1f891a1030bd1d1eb9092268ed0b63eefb266210
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-04-09 14:15:40 +0530

    DSA Sheet R&B

---
+++ pattern_wise_practice/recursion&backtrack/lpsstring.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string lcs(string p,string q,int n,int m){
+    if(n==0 || m==0 ){
+        return "";
+    }
+    
+    if(p[n-1]==q[m-1]){
+        return lcs(p,q,n-1,m-1)+p[n-1];
+    }
+    else{
+        string s1=lcs(p,q,n-1,m);
+        string s2=lcs(p,q,n,m-1);
+        return (s1.size()>s2.size())?s1:s2;
+    }
+
+}
+int main(){
+    string p="agbcba",q="abcbga";
+    
+    cout<<lcs(p,q,p.size(),q.size());
+    return 0;
+}
