f419937bb38fea76c25b27ea9834222bc740b9c9
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-04-14 21:09:32 +0530

    DSA Sheet R&B

--- pattern_wise_practice/recursion&backtrack/climbstairs.cpp
+++ pattern_wise_practice/recursion&backtrack/climbstairs.cpp
@@ -1,15 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int helper(int n)
+int helper(int n,vector<int>&dp)
 {
     if (n == 0 || n == 1)
         return 1;
-
-    return helper(n - 1) + helper(n - 2);
+    if(dp[n]!=-1)return dp[n];
+    return dp[n]=helper(n - 1,dp) + helper(n - 2,dp);
 }
 int main()
-{
-    cout<<helper(3);
+{   int n=37;
+    vector<int>dp(n+1,-1);
+    cout<<helper(n,dp);
     return 0;
 }
