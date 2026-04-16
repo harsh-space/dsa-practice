8b886808d2150186928a7fb2f3ef29663895ba49
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-04-16 09:56:32 +0530

    DSA Sheet R&B

--- pattern_wise_practice/recursion&backtrack/mincoststairs.cpp
+++ pattern_wise_practice/recursion&backtrack/mincoststairs.cpp
@@ -1,11 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int helper(int i,vector<int>&cost){
-    if(i>=cost.size())return 0;
-    return cost[i]+min(helper(i+1,cost),helper(i+2,cost));
+int helper(int i, vector<int> &cost, vector<int> &dp)
+{
+    if (i >= cost.size())
+        return 0;
+    if (dp[i] != -1)
+        return dp[i];
+    return dp[i] = cost[i] +
+                   min(helper(i + 1, cost, dp), helper(i + 2, cost, dp));
 }
-int main(){
-    vector<int>cost={1,100,1,1,1,100,1,1,100,1};
-    cout<<min(helper(0,cost),helper(1,cost));
+int main()
+{
+    vector<int> cost = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+    int n = cost.size();
+    vector<int> dp(n + 1, -1);
+    cout<< min(helper(0, cost, dp), helper(1, cost, dp));
     return 0;
 }
