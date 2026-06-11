3527b71d82f1e7c803c2a60a7a1595b7ead1bab4
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-06-11 14:16:25 +0530

    Contest 449: C partially done

---
+++ atcoder_contests/449/c.cpp
@@ -0,0 +1,98 @@
+/*
+    C - Comfortable Distance
+
+    Time Limit: 2 sec
+    Memory Limit: 1024 MiB
+
+    Score:
+    300 points
+
+
+    Problem Statement
+    -----------------
+    You are given a string S of length N consisting of lowercase English letters.
+
+    Find the number of pairs of integers (i, j) satisfying all of the following conditions:
+
+        1 ≤ i ≤ j ≤ N
+        S[i] = S[j]
+        L ≤ (j - i) ≤ R
+
+
+    Constraints
+    -----------
+        2 ≤ N ≤ 5 × 10^5
+        1 ≤ L ≤ R ≤ N - 1
+
+        N, L, and R are integers.
+        S is a string of length N consisting of lowercase English letters.
+
+
+    Input
+    -----
+    The input is given from Standard Input in the following format:
+
+        N L R
+        S
+
+
+    Output
+    ------
+    Output the answer.
+
+
+    Sample Input 1
+    --------------
+        6 2 4
+        aabcba
+
+    Sample Output 1
+    ---------------
+        2
+
+    The pairs satisfying the conditions are:
+
+        (i, j) = (2, 6), (3, 5)
+
+    Hence, the answer is 2.
+
+
+    Sample Input 2
+    --------------
+        9 3 6
+        aaaaaaaaa
+
+    Sample Output 2
+    ---------------
+        18
+
+
+    Sample Input 3
+    --------------
+        10 2 6
+        aabbccaabb
+
+    Sample Output 3
+    ---------------
+        6
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n,l,r;
+    string s;
+
+    cin>>n>>l>>r;
+    cin>>s;
+    long long cnt=0;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(s[i]==s[j] && (j-i<=r && j-i>=l))cnt++;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
