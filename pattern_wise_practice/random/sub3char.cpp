a441d407b1918ee32438f3ce91bd76448554d56f
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-06-30 23:10:58 +0530

    DSA Sheet RANDOM

---
+++ pattern_wise_practice/random/sub3char.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int numberOfSubstrings(string s)
+{
+    int n = s.size();
+    vector<int> freq(3, 0);
+    int left = 0, cnt = 0;
+
+    for (int right = 0; right < n; right++)
+    {
+        freq[s[right] - 'a']++;
+
+        while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0)
+        {
+            cnt += (n - right);
+            freq[s[left] - 'a']--;
+            left++;
+        }
+    }
+    return cnt;
+}
+int main()
+{
+    string s="aaabc";
+    cout<<numberOfSubstrings(s);
+    return 0;
+}
