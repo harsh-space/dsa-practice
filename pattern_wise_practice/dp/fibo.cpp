eff4f697806e774cdf0837ba37fe490de097652d
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-08-22 10:04:27 +0530

    DSA Sheet DP

--- pattern_wise_practice/dp/fibo.cpp
+++ pattern_wise_practice/dp/fibo.cpp
@@ -1,15 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-int fibo(int n,vector<int>&dp){
-    if(n<=1)return n;
-    if(dp[n]!=-1)return dp[n];
-    return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
-}
-int main(){
+// int fibo(int n,vector<int>&dp){
+//     if(n<=1)return n;
+//     if(dp[n]!=-1)return dp[n];
+//     return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+// }
+// int main(){
 
+//     int n;
+//     cin>>n;
+//     vector<int>dp(n+1,-1);
+//     cout<<fibo(n,dp);
+//     return 0;
+// }
+
+int main(){
     int n;
     cin>>n;
     vector<int>dp(n+1,-1);
-    cout<<fibo(n,dp);
+    dp[0]=0,dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    cout<<dp[n];
     return 0;
 }
