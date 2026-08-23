8426516e153e18e300f4808842822d6ad4c6e0bb
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-08-24 03:21:47 +0530

    DSA Sheet DP

---
+++ pattern_wise_practice/dp/unipaths.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int paths(int i, int j, int m, int n, vector<vector<int>>& dp) {
+    if (i == m - 1 && j == n - 1) return 1;
+    if (i >= m || j >= n) return 0;
+    if (dp[i][j] != -1) return dp[i][j];
+    
+    return dp[i][j] = paths(i + 1, j, m, n, dp) + paths(i, j + 1, m, n, dp);
+}
+
+int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m, vector<int>(n, -1));
+    return paths(0, 0, m, n, dp);
+}
+
+int main() {
+    int m = 3;
+    int n = 7;
+    
+    int result = uniquePaths(m, n);
+    
+    cout << "Number of unique paths: " << result << endl;
+    
+    return 0;
+}
