ca4ebc013433fbce6e5b3810545535a641ace478
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-09-05 03:15:11 +0530

    DSA Sheet DP

--- pattern_wise_practice/dp/lps.cpp
+++ pattern_wise_practice/dp/lps.cpp
@@ -1,14 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int lps(int i,int j,string&s){
-    if(i>j)return 0;
-    if(i==j)return 1;
-    if(s[i]==s[j])return 2+lps(i+1,j-1,s);
-    return max(lps(i+1,j,s),lps(i,j-1,s));
+
+int lps(int i, int j, const string& s, vector<vector<int>>& dp) {
+    if (i > j) return 0;
+
+    if (i == j) return 1;
+
+    if (dp[i][j] != -1) return dp[i][j];
+    if (s[i] == s[j]) {
+        dp[i][j] = 2 + lps(i + 1, j - 1, s, dp);
+    } 
+
+    else {
+        dp[i][j] = max(lps(i + 1, j, s, dp), lps(i, j - 1, s, dp));
+    }
+    
+    return dp[i][j];
 }
-int main(){
-    string s="ssabbxcdss";
-    int i=0,j=s.size()-1;
-    cout<<lps(i,j,s);
+
+int main() {
+    string s = "ssabbxcdss";
+    int n = s.size();
+    vector<vector<int>> dp(n, vector<int>(n, -1));
+    
+    cout << lps(0, n - 1, s, dp) << endl; 
+    
     return 0;
 }
